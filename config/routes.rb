9effc6d22c8704a73c352bd4d3a87dc11d6f0f0e
rinuan <rinuan2014@gmail.com>
2014-06-19 09:59:55 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Groupme::Application.routes.draw do
+  resources :groups
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  root :to => "groups#index"
+end
---
+++ groupme/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupme::Application.config.secret_key_base = 'd78cfa8ccb7d7f7e36db15b196f8b4c4c7327b9e4e4c963684367c0f8067f665083800165c5988d652a656666dfbd513db1336f04c0617f4423498712e156687'
